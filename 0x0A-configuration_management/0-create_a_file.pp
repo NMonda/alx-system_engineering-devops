c0e0a00d203449bed2d2a88e8dc970312898cecb
NMonda <ndegemonda@gmail.com>
2022-09-21 23:04:20 +0300

    bug fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
-# Using Puppet, create a file in /tmp.
-
+# Using Puppet, create a file in /tmp directory
 file { '/tmp/holberton':
-  ensure  => file,
+  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
